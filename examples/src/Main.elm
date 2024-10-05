47155161db0bb2419204fe6c57694f803d97a452
Ed <edwardjkelly@gmail.com>
2024-10-05 11:34:40 +0100

    Fiddle with examples

--- examples/src/Main.elm
+++ examples/src/Main.elm
@@ -25,6 +25,7 @@ type alias Model =
 
 type Msg
     = TimerExpired
+    | TimerReset
 
 
 app =
@@ -34,29 +35,62 @@ app =
             \msg model ->
                 case msg of
                     TimerExpired ->
-                        ( { timerExpired = True }, Cmd.none )
+                        ( { model | timerExpired = True }, Cmd.none )
+
+                    TimerReset ->
+                        ( { model | timerExpired = False }, Cmd.none )
         , view =
             \counterButton_ sendToSelf model ->
                 Html.div []
                     [ Html.p []
-                        [ Html.h4 [] [ Html.text "The timer element below comes from an encapsulated component, which manages its own state and can send messages to the app's update function" ]
-                        , Html.div [] [ counterButton_.view ]
+                        [ Html.text
+                            """
+                            The timer element below comes from an 
+                            encapsulated component, which manages its own 
+                            state, completely separately from the app's 
+                            model. It can send messages to the app's update 
+                            function, and receive messages sent by the app's 
+                            view function.
+                            """
                         ]
-                    , Html.p
-                        []
-                        [ Html.h4 [] [ Html.text "Here we send a message from the app's view to the component" ]
-                        , Html.button
-                            [ Html.Events.onClick (counterButton_.send Reset) ]
-                            [ Html.text "Reset timer" ]
-                        ]
+                    , counterButton_.view
                     , Html.p []
-                        [ Html.h4 [] [ Html.text "And here's a look at our app's model - gloriously unpolluted by component state" ]
-                        , Html.text (Debug.toString model)
+                        [ Html.text
+                            """
+                            Here is a `Debug.toString` of the app's model:
+                            """
                         ]
+                    , Html.p [ Html.Attributes.style "font-family" "monospace" ]
+                        [ Html.text (Debug.toString model) ]
+                    , Html.p []
+                        [ Html.text
+                            """
+                            The only thing the app's model does is keep track of
+                            whether the timer has expired. But as you can see, 
+                            it doesn't (and can't) know anything about the 
+                            actual state of the timer - whether it's ticking, 
+                            how much time is left, etc. The only way it can find 
+                            out whether the timer has expired is if it receives 
+                            a message from the timer component. So, the timer's
+                            update function is configured to send a message to
+                            the app's update function when it reaches zero
+                            """
+                        ]
+
+                    , Html.p []
+                        [ Html.text
+                            """
+                            Here we have a button in the app's view function 
+                            that can send a message to the timer component
+                            """
+                        ]
+                    , Html.button
+                        [ Html.Events.onClick (counterButton_.send Reset) ]
+                        [ Html.text "Reset timer" ]
                     ]
         , subscriptions = \model -> Sub.none
         }
-        |> App.add (timer { timerExpired = TimerExpired })
+        |> App.add (timer { timerExpired = TimerExpired, timerReset = TimerReset })
         |> App.done
 
 
@@ -66,7 +100,7 @@ type TimerMsg
     | Reset
 
 
-timer { timerExpired } =
+timer { timerExpired, timerReset } =
     { init = Nothing
     , update =
         \cmd msg model ->
@@ -76,18 +110,26 @@ timer { timerExpired } =
 
                 Tick ->
                     if model == Just 0 then
-                        ( Nothing, cmd.toParent (Task.perform (\_ -> timerExpired) (Process.sleep 0)) )
+                        ( Just 0, cmd.toParent (Task.perform (\_ -> timerExpired) (Process.sleep 0)) )
 
                     else
                         ( Maybe.map (\n -> n - 1) model, Cmd.none )
 
                 Reset ->
-                    ( Nothing, cmd.toParent (Task.perform (\_ -> timerExpired) (Process.sleep 0)) )
+                    ( Nothing, cmd.toParent (Task.perform (\_ -> timerReset) (Process.sleep 0)) )
     , view =
         \model ->
-            Html.article []
-                [ Html.strong [] [ Html.text "Countdown timer" ]
-                , Html.h2 [] [ Html.text (model |> Maybe.withDefault 10 |> String.fromInt) ]
+            Html.article
+                [ Html.Attributes.style "border" "solid 1px pink"
+                , Html.Attributes.style "border-radius" "10px"
+                , Html.Attributes.style "background-color" "aliceblue"
+                , Html.Attributes.style "padding" "10px"
+                , Html.Attributes.style "width" "200px"
+                , Html.Attributes.style "text-align" "center"
+                , Html.Attributes.style "font-family" "sans-serif"
+                ]
+                [ Html.p [] [ Html.text "Countdown timer" ]
+                , Html.h1 [] [ Html.text (model |> Maybe.withDefault 10 |> String.fromInt) ]
                 , Html.button
                     [ Html.Events.onClick Start ]
                     [ Html.text "Start" ]
@@ -103,23 +145,4 @@ timer { timerExpired } =
 
                 Just _ ->
                     Time.every 1000 (\_ -> Tick)
-    }
-
-
-type FloatMsg
-    = FloatChanged String
-
-
-floatInput =
-    { init = ""
-    , update =
-        \cmd msg model ->
-            case msg of
-                FloatChanged str ->
-                    ( str, Cmd.none )
-    , view =
-        \model ->
-            Html.input [ Html.Events.onInput FloatChanged ] []
-    , subscriptions =
-        \model -> Sub.none
     }
